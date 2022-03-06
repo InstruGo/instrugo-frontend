0c07c631792d4c85b17cbb742cdce4cd8f44e2f2
ftodoric <fptodoric@gmail.com>
2022-03-06 21:21:43 +0100

    Setup react-intl for localization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,25 @@ import type { AppProps } from 'next/app';
 
 import '../styles/reset.css';
 
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+
+import { locales } from '../localization/messages';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Determine active locale and set messages
+  const { locale, defaultLocale } = useRouter();
+  const messages = locale ? locales[locale] : locales['en'];
+
+  return (
+    <IntlProvider
+      locale={locale ? locale : 'en'}
+      defaultLocale={defaultLocale}
+      messages={messages}
+    >
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
 export default MyApp;
