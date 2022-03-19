f7dc6c91e785ad95755a71adc4c9af92b9c5b483
Lara <lara.granosa@fer.hr>
2022-03-19 14:18:38 +0100

    validation for pass removed

--- src/types/login.type.ts
+++ src/types/login.type.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const loginFormSchema = z.object({
   email: z.string().email('Unesite ispravnu e-mail adresu!'),
-  password: z.string().min(6, 'Neispravna lozinka!'),
+  password: z.string(),
 });
 
 export type LoginFormInputs = z.infer<typeof loginFormSchema>;
