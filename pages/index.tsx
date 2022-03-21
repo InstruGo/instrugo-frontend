ac1eec25e1a85bc04ebb4e02d9bfb61e34979d99
ftodoric <fptodoric@gmail.com>
2022-03-21 23:14:42 +0100

    Remove modal from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import { Modal } from '@components';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
 
 const Home: NextPage = () => {
-  const [shouldShow, setShouldShow] = React.useState(false);
-
   return (
     <div>
       <Head>
@@ -13,14 +10,6 @@ const Home: NextPage = () => {
           InstruGo | Get help with math, physics, languages and more
         </title>
       </Head>
-
-      <button onClick={() => setShouldShow(true)}>details</button>
-      <Modal
-        shouldShow={shouldShow}
-        onBackgroundClick={() => setShouldShow(false)}
-      >
-        <div>this is details modal</div>
-      </Modal>
     </div>
   );
 };
