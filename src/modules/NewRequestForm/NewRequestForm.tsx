8d140f9a308c4bfc9f51f5eccb5b128ddb484377
ftodoric <fptodoric@gmail.com>
2022-04-20 19:23:38 +0200

    Revert newRequestForm

--- src/modules/NewRequestForm/NewRequestForm.tsx
+++ src/modules/NewRequestForm/NewRequestForm.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { FaPlus } from 'react-icons/fa';
 import { FormattedMessage } from 'react-intl';
 
 import { Input } from '@components';
 import { Button, TimeSlot } from '@components';
 import { useNewRequest, useSubjects, useUserContext } from '@hooks';
-
 import {
   NewRequestFormInputs,
   newRequestFormSchema,
@@ -16,21 +14,20 @@ import {
   MeetingType,
 } from '../../types/new-request.type';
 import {
-  FormContainer,
+  NewRequestFormContainer,
   InputDescription,
   RadioInput,
   TextBox,
   FormColumn,
-  InputsContainer,
+  FormRow,
+  NewRequestText,
   Dropdown,
   DropdownOption,
-  NewRequestTitle,
 } from './styles';
 
 interface NewRequestProps {
   onFinish?: () => void;
 }
-
 export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
   const { data, isLoading } = useSubjects();
   const { user } = useUserContext();
@@ -43,31 +40,24 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
   } = useForm<NewRequestFormInputs>({
     resolver: zodResolver(newRequestFormSchema),
   });
-
   const onSubmit = async (data: NewRequestFormInputs) => {
     newRequest.mutate(data);
-
     if (onFinish) {
       onFinish();
     }
   };
-
   const [selectedEducationLevel, setEducationLevel] = useState<EducationLevel>(
     EducationLevel.ELEMENTARY
   );
-
   const [selectedMeetingType, setMeetingType] = useState<MeetingType>(
     MeetingType.IRL
   );
-
   const onLevelSelect = (e: any) => {
     setEducationLevel(e.target.value);
   };
-
   const onMeetingSelect = (e: any) => {
     setMeetingType(e.target.value);
   };
-
   const [slotCount, setSlotCount] = useState(0);
   const [timeSlots, updateTimeSlots] = useState([
     {
@@ -77,7 +67,6 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
       endTime: new Date(),
     },
   ]);
-
   const updateLessonTimeFrames = (tempSlots: any) => {
     const lessonTimeFrames = [];
     for (let i = 0; i < tempSlots.length; i++) {
@@ -86,19 +75,16 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
         tempSlots[i].date.getDate(),
         tempSlots[i].date.getFullYear(),
       ];
-
       const [hourStart, minutesStart, secondsStart] = [
         tempSlots[i].startTime.getHours(),
         tempSlots[i].startTime.getMinutes(),
         tempSlots[i].startTime.getSeconds(),
       ];
-
       const [hourEnd, minutesEnd, secondsEnd] = [
         tempSlots[i].endTime.getHours(),
         tempSlots[i].endTime.getMinutes(),
         tempSlots[i].endTime.getSeconds(),
       ];
-
       const start = new Date(
         year,
         month,
@@ -107,17 +93,14 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
         minutesStart,
         secondsStart
       );
-
       const end = new Date(year, month, day, hourEnd, minutesEnd, secondsEnd);
       lessonTimeFrames.push({
         startTime: start.toISOString(),
         endTime: end.toISOString(),
       });
     }
-
     setValue('lessonTimeFrames', lessonTimeFrames);
   };
-
   const onDateChange = (
     idx: number,
     date: Date,
@@ -132,11 +115,9 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
         tempSlots[i].endTime = endTime;
       }
     }
-
     updateTimeSlots(tempSlots);
     updateLessonTimeFrames(tempSlots);
   };
-
   const destroyTimeSlot = (index: number) => {
     let tempSlots = timeSlots;
     for (let i = 0; i < tempSlots.length; i++) {
@@ -144,10 +125,8 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
         tempSlots.splice(i, 1);
       }
     }
-
     updateTimeSlots(tempSlots);
   };
-
   const [timeSlotList, updateTimeSlotList] = useState([
     <li key={0}>
       <TimeSlot
@@ -157,7 +136,6 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
       />
     </li>,
   ]);
-
   const onAddTimeSlot = () => {
     let tempSlots = timeSlots;
     setSlotCount(slotCount + 1);
@@ -167,7 +145,6 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
       startTime: new Date(),
       endTime: new Date(),
     });
-
     updateTimeSlots(tempSlots);
     let tempSlotList = timeSlotList;
     tempSlotList.push(
@@ -179,16 +156,14 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
         />
       </li>
     );
-
     updateTimeSlotList(tempSlotList);
   };
 
   updateLessonTimeFrames(timeSlots);
-  const [subjectId, setSubjectId] = useState<number>(0);
+  const [subjectId, setSubjectId] = useState<number>(1);
   setValue('subjectId', subjectId);
 
   if (isLoading) return <div>Loading...</div>;
-
   if (user) {
     setValue('userId', user.id);
   } else {
@@ -197,169 +172,153 @@ export const NewRequestForm = ({ onFinish }: NewRequestProps) => {
 
   return (
     <>
-      <NewRequestTitle>
+      <NewRequestText>
         <FormattedMessage id="newRequest.description" />
-      </NewRequestTitle>
+      </NewRequestText>
 
-      <FormContainer onSubmit={handleSubmit(onSubmit)}>
-        <InputsContainer>
+      <NewRequestFormContainer onSubmit={handleSubmit(onSubmit)}>
+        <FormRow>
           <FormColumn>
             <InputDescription>
-              <FormattedMessage id="card.subject" />:
-              <Dropdown
-                value={subjectId}
-                onChange={(e) => {
-                  setSubjectId(Number.parseInt(e.target.value));
-                  setValue('subjectId', Number.parseInt(e.target.value));
-                }}
-              >
-                {data?.map((subject) => (
-                  <DropdownOption key={subject.id} value={subject.id}>
-                    {subject.name}
-                  </DropdownOption>
-                ))}
-              </Dropdown>
+              <FormattedMessage id="card.subject" />:{' '}
             </InputDescription>
+            <Dropdown
+              value={subjectId}
+              onChange={(e) => {
+                setSubjectId(parseInt(e.target.value, 10));
+                setValue('subjectId', parseInt(e.target.value, 10));
+              }}
+            >
+              {data?.map((subject) => (
+                <DropdownOption key={subject.id} value={subject.id}>
+                  {subject.name}
+                </DropdownOption>
+              ))}
+            </Dropdown>
 
             <InputDescription>
               <FormattedMessage id="card.subfield" />:
-              <Input
-                name="subfield"
-                placeholder="eg. Math"
-                register={register}
-                errors={errors.subfield}
-                style={{ width: '120px' }}
-              />
             </InputDescription>
+            <Input
+              name="subfield"
+              register={register}
+              errors={errors.subfield}
+            />
 
-            <InputDescription variant="column">
+            <InputDescription>
               <FormattedMessage id="newRequestForm.educationLevel" />:
-              <RadioInput>
-                <input
-                  {...register('level', { required: true })}
-                  type="radio"
-                  value={EducationLevel.ELEMENTARY}
-                  checked={selectedEducationLevel === EducationLevel.ELEMENTARY}
-                  onChange={onLevelSelect}
-                />
-                {EducationLevel.ELEMENTARY}
-              </RadioInput>
-              <RadioInput>
-                <input
-                  {...register('level', { required: true })}
-                  type="radio"
-                  value={EducationLevel.HIGH}
-                  checked={selectedEducationLevel === EducationLevel.HIGH}
-                  onChange={onLevelSelect}
-                />
-                {EducationLevel.HIGH}
-              </RadioInput>
-              <RadioInput>
-                <input
-                  {...register('level', { required: true })}
-                  type="radio"
-                  value={EducationLevel.UNI}
-                  checked={selectedEducationLevel === EducationLevel.UNI}
-                  onChange={onLevelSelect}
-                />
-                {EducationLevel.UNI}
-              </RadioInput>
             </InputDescription>
+            <RadioInput>
+              <input
+                {...register('level', { required: true })}
+                type="radio"
+                value={EducationLevel.ELEMENTARY}
+                checked={selectedEducationLevel === EducationLevel.ELEMENTARY}
+                onChange={onLevelSelect}
+              />
+              {EducationLevel.ELEMENTARY}
+            </RadioInput>
+            <RadioInput>
+              <input
+                {...register('level', { required: true })}
+                type="radio"
+                value={EducationLevel.HIGH}
+                checked={selectedEducationLevel === EducationLevel.HIGH}
+                onChange={onLevelSelect}
+              />
+              {EducationLevel.HIGH}
+            </RadioInput>
+            <RadioInput>
+              <input
+                {...register('level', { required: true })}
+                type="radio"
+                value={EducationLevel.UNI}
+                checked={selectedEducationLevel === EducationLevel.UNI}
+                onChange={onLevelSelect}
+              />
+              {EducationLevel.UNI}
+            </RadioInput>
 
             <InputDescription>
               <FormattedMessage id="newRequestForm.grade" />:{' '}
-              <Input
-                type="number"
-                name="grade"
-                placeholder="0"
-                register={register}
-                errors={errors.grade}
-                isNumber={true}
-                style={{ width: '60px' }}
-              />
             </InputDescription>
+            <Input
+              type="number"
+              name="grade"
+              register={register}
+              errors={errors.grade}
+              isNumber={true}
+            />
 
             <InputDescription>
-              <FormattedMessage id="newRequestForm.budget" />:
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Input
-                  type="number"
-                  name="budget"
-                  placeholder="0"
-                  register={register}
-                  errors={errors.budget}
-                  isNumber={true}
-                  style={{ width: '80px' }}
-                />
-                <div style={{ marginLeft: '8px' }}>kn/h</div>
-              </div>
+              <FormattedMessage id="newRequestForm.budget" />: (kn/h)
             </InputDescription>
+            <Input
+              type="number"
+              name="budget"
+              register={register}
+              errors={errors.budget}
+              isNumber={true}
+            />
           </FormColumn>
 
           <FormColumn>
-            <InputDescription variant="column">
+            <InputDescription>
               <FormattedMessage id="newRequestForm.availableDates" />:
-              <ul>{timeSlotList}</ul>
-              <Button
-                type="button"
-                onClick={onAddTimeSlot}
-                style={{
-                  alignSelf: 'flex-end',
-                  padding: '7px',
-                }}
-              >
-                <FaPlus size="16px" />
-              </Button>
             </InputDescription>
-
-            <InputDescription variant="column">
-              <FormattedMessage id="card.meetingType" />:
-              <RadioInput>
-                <input
-                  {...register('type', { required: true })}
-                  type="radio"
-                  value={MeetingType.IRL}
-                  checked={selectedMeetingType === MeetingType.IRL}
-                  onChange={onMeetingSelect}
-                />
-                {MeetingType.IRL}
-              </RadioInput>
-              <RadioInput>
-                <input
-                  {...register('type', { required: true })}
-                  type="radio"
-                  value={MeetingType.ONLINE}
-                  checked={selectedMeetingType === MeetingType.ONLINE}
-                  onChange={onMeetingSelect}
-                />
-                {MeetingType.ONLINE}
-              </RadioInput>
-            </InputDescription>
+            <ul>{timeSlotList}</ul>
+            <Button type="button" onClick={onAddTimeSlot}>
+              + <FormattedMessage id="newRequestForm.newTime" />
+            </Button>
 
             <InputDescription>
-              <FormattedMessage id="card.location" />:
-              <Input
-                name="location"
-                placeholder="eg. zagreb, croatia"
-                register={register}
-                errors={errors.location}
-                style={{ marginLeft: '50px' }}
-              />
+              <FormattedMessage id="card.meetingType" />:
             </InputDescription>
-
-            <div style={{}}>
-              <FormattedMessage id="newRequestForm.additionalInfo" />:{' '}
-            </div>
+            <RadioInput>
+              <input
+                {...register('type', { required: true })}
+                type="radio"
+                value={MeetingType.IRL}
+                checked={selectedMeetingType === MeetingType.IRL}
+                onChange={onMeetingSelect}
+              />
+              {MeetingType.IRL}
+            </RadioInput>
+            <RadioInput>
+              <input
+                {...register('type', { required: true })}
+                type="radio"
+                value={MeetingType.ONLINE}
+                checked={selectedMeetingType === MeetingType.ONLINE}
+                onChange={onMeetingSelect}
+              />
+              {MeetingType.ONLINE}
+            </RadioInput>
+            <InputDescription>
+              <FormattedMessage id="card.location" />:
+            </InputDescription>
+            <Input
+              name="location"
+              register={register}
+              errors={errors.location}
+            />
+            <InputDescription>
+              <FormattedMessage id="newRequestForm.description" />:{' '}
+            </InputDescription>
             <TextBox {...register('description', { required: true })} />
           </FormColumn>
-        </InputsContainer>
+        </FormRow>
 
-        <Input
-          type="submit"
-          variant="authSubmit"
-          placeholderMsgId="button.createRequest"
-        />
-      </FormContainer>
+        <FormRow>
+          <FormColumn style={{ alignItems: 'center' }}>
+            <Input
+              type="submit"
+              variant="authSubmit"
+              placeholderMsgId="button.createRequest"
+            />
+          </FormColumn>
+        </FormRow>
+      </NewRequestFormContainer>
     </>
   );
 };
