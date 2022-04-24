abb17e44f60b6382fcba2ccdf8b909c33839017a
Lara <lara.granosa@fer.hr>
2022-04-24 11:06:13 +0200

    tutors homepage

--- pages/tutor/home.tsx
+++ pages/tutor/home.tsx
@@ -1,10 +1,37 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { withAuth } from '@components';
+import { LessonsContainer, TutorsNavbar } from '@modules';
+import { styled } from 'styles/stitches.config';
+
+const PageLayout = styled('div', {
+  padding: '20px',
+  '> div + div': {
+    marginTop: '100px',
+  },
+});
 
 const TutorHomepage: NextPage = () => {
-  return <div>tutor home</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <Head>
+        <title>
+          InstruGo | Get help with math, physics, languages and more
+        </title>
+      </Head>
+
+      <TutorsNavbar />
+
+      <PageLayout>
+        <LessonsContainer title={'tutorsHome.upcoming'} cards />
+        <LessonsContainer title={'tutorsHome.recommended'} cards filter />
+      </PageLayout>
+    </div>
+  );
 };
 
 export default withAuth(TutorHomepage);
